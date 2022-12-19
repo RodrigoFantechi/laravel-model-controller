f5e4497592f7b4c9e68f58fd068b7112c4a25387
RodrigoFantechi <rodrifante595@libero.it>
2022-12-19 14:54:34 +0100

    add header and footer

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
